b6277397ef2fd03f8fd09811596741f842f24d48
based.kaweya <158149366+based-kaweya@users.noreply.github.com>
2024-04-20 17:51:57 +0530

    layout is added

---
+++ src/layout/partials/Header.comp.js
@@ -0,0 +1,29 @@
+// headercomp.js
+
+import React from "react";
+import { Navbar, Nav, NavbarBrand } from "react-bootstrap";
+import logo from "../../assests/img/logo.jpg";
+
+export const Header = () => {
+  const linkStyle = {
+    color: "rgba(255, 229, 153, 1)",
+    padding: "10px 15px",
+    marginRight: "10px",
+  };
+
+  return (
+    <Navbar collapseOnSelect expand="md" style={{ backgroundColor: "rgba(12, 52, 61, 1)" }}>
+      <Navbar.Brand>
+        <img src={logo} alt="logo" width="60px" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/dashboard" style={linkStyle}>Dashboard</Nav.Link>
+          <Nav.Link href="/tickets" style={linkStyle}>Tickets</Nav.Link>
+          <Nav.Link href="/logout" style={linkStyle}>Logout</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
